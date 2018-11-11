d21d81634833c548585e2ae02c1a053477ed62de
Sathaphorn Sunthornpan <sathaphorn.stp@gmail.com>
2018-11-11 14:43:44 +0700

    add token configuration properties

---
+++ src/main/java/com/example/demo/GithubProperties.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import javax.validation.constraints.Pattern;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * @author sathaphorn.stp (T.STP)
+ * @since 11-11-2018, 14:21
+ */
+@ConfigurationProperties("github")
+@Validated
+public class GithubProperties {
+
+  /**
+   * Github API token ("user:sampleToken")
+   */
+  @Pattern(regexp = "\\w+:\\w+")
+  private String token;
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+}
